c70afd210604432302b10758b59b051b8ebb74f3
Elena <agapitovae87@gmail.com>
2023-02-19 22:16:57 +0300

    Task1 hw4 changed

--- homeworks/homework3/Task1.java
+++ homeworks/homework3/Task1.java
@@ -23,10 +23,7 @@ public class Task1 {
     }
 
     static void mergeSortArray(int[] array) {
-        if (array.length == 1) {
-            return;
-        }
-
+        if (array.length == 1) return;
         int mid = array.length / 2;
         int[] left = Arrays.copyOfRange(array, 0, mid);
         int[] right = Arrays.copyOfRange(array, mid, array.length);
@@ -36,27 +33,12 @@ public class Task1 {
     }
 
     static void sortArray(int[] array, int[] left, int[] right) {
-        int i = 0,  j = 0, k = 0;
+        int i = 0, j = 0, k = 0;
         while (i < left.length && j < right.length) {
-            if (left[i] <= right[j]) {
-                array[k] = left[i];
-                k++;
-                i++;
-            } else {
-                array[k] = right[j];
-                k++;
-                j++;
-            }
+            if (left[i] <= right[j]) array[k++] = left[i++];
+            else array[k++] = right[j++];
         }
-        while (i < left.length) {
-            array[k] = left[i];
-            k++;
-            i++;
-        }
-        while (j < right.length) {
-            array[k] = right[j];
-            k++;
-            j++;
-        }
+        while (i < left.length) array[k++] = left[i++];
+        while (j < right.length) array[k++] = right[j++];
     }
 }
